66dce8709199d9777b0191f34588f332e1c23938
HughParsonage <hugh.parsonage@gmail.com>
2016-10-11 14:10:55 +1100

    For newfilename

--- CheckBiber.R
+++ CheckBiber.R
@@ -1,2 +1,2 @@
-if (any(grepl("WARN", readLines("Better-targeting-super-breaks.blg"))))
+if (any(grepl("WARN", readLines("Assessing-the-2016-super-tax-reforms.blg")) && !grepl("2000--2015", readLines("Assessing-the-2016-super-tax-reforms.blg"))))
   stop("biber emitted warnings.")
